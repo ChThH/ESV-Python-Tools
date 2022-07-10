6f7e4fab757dff496a862c5c7ea617a8f55dd724
CT Hall <cthall83@gmail.com>
2022-07-09 19:16:53 -0700

    Update readables.py
    updated

--- readables.py
+++ readables.py
@@ -1,19 +1,29 @@
+"""
+Books and chapters of the Readables/Deuterocannonicals/Apocrypha
+"""
+
 readables = [None,
-			('Tobit', 14, [None, 22, 14, 17, 21, 22, 18, 17, 21, 6, 13, 19, 22, 18, 15]),
-			('Judith', 16, [None, 16, 28, 10, 15, 24, 21, 32, 36, 14, 23, 23, 20, 20, 19, 13, 25]),
-			('Esther (Greek)', 16, [None, 22, 23, 15, 17, 14, 14, 10, 17, 31, 13, 12, 6, 18, 19, 16, 24]),
-			('Wisdom of Solomon', 19, [None, ]),
-			('Sirach', 51, [1, ]),
-			('Baruch', 5, [None, ]),
-			('Letter of Jeremiah', 1, [None, 73]),
-			('Prayer of Azariah and the Song of the Three Young Men', 1, [None, 68]), 
-			('Susanna', 1, [None, 64]),
-			('Bel and the Dragon', 1, [None, 42]),
-			('1 Maccabees', 16, [None, ]),
-			('2 Maccabees', 15, [None, ]),
-			('1 Esdras', 9, [None, ]),
-			('Prayer of Manasseh', 1, [None, 15]),
-			('Psalm 151', 1 [None, 7]),
-			('3 Maccabees', 7, [None, ]),
-			('2 Esdras', 16, [None, ]),
-			('4 Maccabees', 18, [None, ])  ]
+             ('Tobit', 14, [None, 22, 14, 17, 21, 22, 18, 17, 21, 6, 13, 19, 22, 18, 15]),
+             ('Judith', 16, [None, 16, 28, 10, 15, 24, 21, 32, 36, 14, 23, 23, 20, 20, 19, 13, 25]),
+             ('Esther (Greek)', 16, [None, 22, 23, 15, 17, 14, 14, 10, 17, 31, 13, 12, 6, 18, 19, 16, 24]),
+             # Chapters in Esther are idiosyncratically numbered/organized.
+             ('Wisdom of Solomon', 19, [None, 16, 24, 19, 20, 23, 25, 30, 21, 18, 21, 26, 27, 19, 31, 19, 29, 21, 25, 
+                                        22]),
+             ('Sirach', 51, [1, 30, 18, 31, 31, 15, 37, 36, 19, 18, 31, 34, 18, 26, 27, 20, 30, 32, 33, 30, 31, 28, 27,
+                             27, 34, 26, 29, 30, 26, 28, 25, 31, 24, 33, 31, 26, 31, 31, 34, 35, 30, 22, 25, 33, 23, 26,
+                             20, 25, 25, 16, 29, 30]),
+             # Sirach's verse numbers are from the longer edition. The Prologue has no verse markings.
+             ('Baruch', 5, [None, 22, 35, 37, 37, 9]),
+             ('Letter of Jeremiah', 1, [None, 73]),
+             ('Prayer of Azariah and the Song of the Three Young Men', 1, [None, 68]),
+             ('Susanna', 1, [None, 64]),
+             ('Bel and the Dragon', 1, [None, 42]),
+             ('1 Maccabees', 16, [None, 64, 70, 60, 61, 68, 63, 50, 32, 73, 89, 74, 53, 53, 49, 41, 24]),
+             ('2 Maccabees', 15, [None, 36, 32, 40, 50, 27, 31, 42, 36, 29, 38, 38, 45, 26, 46, 39]),
+             ('1 Esdras', 9, [None, 58, 30, 24, 63, 73, 34, 15, 96, 55]),
+             ('Prayer of Manasseh', 1, [None, 15]),
+             ('Psalm 151', 1[None, 7]),
+             ('3 Maccabees', 7, [None, 29, 33, 30, 21, 51, 41, 23]),
+             ('2 Esdras', 16, [None, 40, 48, 35, 52, 56, 59, 140, 63, 47, 59, 46, 51, 58, 48, 63, 78]),
+             ('4 Maccabees', 18, [None, 35, 24, 21, 26, 38, 35, 23, 29, 32, 21, 27, 19, 27, 20, 32, 25, 24, 24])
+             ]
